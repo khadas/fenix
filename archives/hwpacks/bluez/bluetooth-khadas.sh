94f220f34dbd27477e4308da5dbdf9d5a7c30c27
nick <nick@khadas.com>
2017-06-29 18:18:13 +0800

    BT: 1. update brcm_patchram_plus, build from brcm_patchram_plus.c 	2. add brcm_patchram_plus source code brcm_patchram_plus.c 	3. update bluetooth-khadas.sh, add AP6356S bluetooth support

--- archives/hwpacks/bluez/bluetooth-khadas.sh
+++ archives/hwpacks/bluez/bluetooth-khadas.sh
@@ -5,10 +5,13 @@ model=`cat /sys/bus/mmc/devices/sdio:0001/sdio:0001:1/device`
 # Load the firmware
 if [ "$model" = "0xa9bf" ]; then
     # VIM Pro
-    /usr/local/bin/brcm_patchram_plus --patchram /lib/firmware/brcm/BCM4345C0.hcd --enable_hci --no2bytes --tosleep 1000 /dev/ttyS1
-else
+	/usr/local/bin/brcm_patchram_plus --patchram /lib/firmware/brcm/BCM4345C0.hcd  --no2bytes --tosleep 1000 /dev/ttyS1
+elif [ "$model" = "0xa9a6" ]; then
     # VIM
-    /usr/local/bin/brcm_patchram_plus --patchram /lib/firmware/brcm/bcm43438a1.hcd --enable_hci --no2bytes --tosleep 1000 /dev/ttyS1
+	/usr/local/bin/brcm_patchram_plus --patchram /lib/firmware/brcm/bcm43438a1.hcd  --no2bytes --tosleep 1000 /dev/ttyS1
+elif [ "$model" = "0x4356" ]; then
+	# VIM2
+	/usr/local/bin/brcm_patchram_plus  --patchram /lib/firmware/brcm/bcm4356a2.hcd --no2bytes --tosleep 1000 /dev/ttyS1
 fi
 
 # FIXME Delay
