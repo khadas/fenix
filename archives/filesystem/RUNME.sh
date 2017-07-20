7d937d30551c95fbb2f0c530f01468f5c0ef0abc
nick <nick@khadas.com>
2017-07-20 17:26:44 +0800

    RUNME.sh: 	1. add admin user 'khadas' 	2. change 'apt' to 'apt-get' 	3. install docker 	4. install kernel & initrd to '/boot'

--- archives/filesystem/RUNME.sh
+++ archives/filesystem/RUNME.sh
@@ -6,6 +6,10 @@
 # Setup password for root user
 echo root:khadas | chpasswd
 
+# Admin user khadas
+useradd -m -p "pal8k5d7/m9GY" -s /bin/bash khadas
+usermod -aG sudo,adm khadas
+
 # Setup host
 echo Khadas > /etc/hostname
 echo "127.0.0.1    localhost.localdomain localhost" > /etc/hosts
@@ -27,22 +31,43 @@ ln -s libEGL.so.1.4 libEGL.so.1
 ln -s libEGL.so.1 libEGL.so
 cd -
 
+sed -i "s/^# deb http/deb http/g" /etc/apt/sources.list
+
 # Fetch the latest package lists from server
-apt update
+apt-get update
 
 # Upgrade
-apt -y upgrade
+apt-get -y --allow-unauthenticated upgrade
 
 # Install the packages
-apt -y install ifupdown net-tools udev fbset vim sudo initramfs-tools \
+apt-get -y --allow-unauthenticated  install ifupdown net-tools udev fbset vim sudo initramfs-tools \
 		bluez rfkill libbluetooth-dev \
 		iputils-ping
+
+# Install Docker
+apt-get -y --allow-unauthenticated install lxc aufs-tools cgroup-lite apparmor docker.io
+usermod -aG docker khadas
 
 # Build the ramdisk
 mkinitramfs -o /boot/initrd.img `cat linux-version` 2>/dev/null
 
+# Generate uInitrd
+mkimage -A arm64 -O linux -T ramdisk -a 0x0 -e 0x0 -n "initrd"  -d /boot/initrd.img  /boot/uInitrd
+
+#Generate uImage
+mkimage -n 'linux-4.9' -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -d /boot/Image /boot/uImage
+
+# Create links
+ln -s /boot/uImage uImage
+ln -s /boot/uInitrd uInitrd
+ln -s /boot/kvim.dtb kvim.dtb
+ln -s /boot/kvim2.dtb kvim2.dtb
+
 # Load WIFI at boot time(MUST HERE)
 echo dhd >> /etc/modules
+
+# Load AUFS module
+echo aufs >> /etc/modules
 
 # Bluetooth
 systemctl enable bluetooth-khadas
