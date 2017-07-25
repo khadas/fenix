53aa590c901512f346a2742216921bbb61d84f06
nick <nick@khadas.com>
2017-07-25 15:01:20 +0800

    Logo: add logo directory

--- scripts/server.sh
+++ scripts/server.sh
@@ -382,6 +382,9 @@ build_rootfs() {
 	cp rootfs/boot/initrd.img images/initrd.img
 	./utils/mkbootimg --kernel linux/arch/arm64/boot/Image --ramdisk images/initrd.img -o images/ramdisk.img
 
+	## Logo
+	cp archives/logo/logo.img images/
+
 	## Clean up
 	sudo rm rootfs/boot/initrd.img
 
