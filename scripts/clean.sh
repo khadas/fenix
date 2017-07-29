7853ab6fd03156d4923ca638eb5e6ef87b7898ad
nick <nick@khadas.com>
2017-07-29 14:01:53 +0800

    clean.sh: update working directory

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-cd ~/project/khadas/ubuntu/
+######################################################################
+
+UBUNTU_WORKING_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+
+
+cd $UBUNTU_WORKING_DIR
 
 ## images
 echo -e "\nCleaning: images..."
 rm -rf images/*.img
 
-## Build U-Boot
+## Clean U-Boot
 echo -e "\nCleaning: U-Boot..."
 cd u-boot/
 make -j8 CROSS_COMPILE=aarch64-linux-gnu- distclean
 
-## Build Linux
+## Clean Linux
 echo -e "\nCleaning: Linux..."
 cd ../linux/
 make -j8 ARCH=arm64 distclean
 
 echo -e "\nDone."
+echo -e "\n`date`"
