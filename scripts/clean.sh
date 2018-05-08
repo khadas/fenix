6ffe9d91b62a504199085a0ce135e18b972461ea
nick <nick@khadas.com>
2018-05-08 14:40:19 +0800

    update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-######################################################################
+set -e -o pipefail
+
+## Parameters
 source config/config
+
+## Board configuraions
+source ${BOARD_CONFIG}/${KHADAS_BOARD}.conf
+
+## Functions
+source config/functions/functions
+#################################################################
 
 cd $ROOT
 
-## images
-echo -e "\nCleaning: images..."
-rm -rf images/*.img
+## Cleanup build images
+info_msg  "Cleanup build images..."
+warning_msg "This script requires root privileges, trying to use sudo, please enter your passowrd!"
+sudo rm -rf $BUILD
+rm -rf $DOWNLOAD_PKG_DIR
 
-## Clean U-Boot
-echo -e "\nCleaning: U-Boot..."
-cd u-boot/
+## Cleanup U-Boot
+info_msg "Cleanup U-Boot..."
+cd $ROOT/u-boot
 make -j8 CROSS_COMPILE=aarch64-linux-gnu- distclean
 
-## Clean Linux
-echo -e "\nCleaning: Linux..."
-cd ../linux/
+## Cleanup Linux
+info_msg "Cleanup Linux..."
+cd $ROOT/linux
 make -j8 ARCH=arm64 distclean
 
 echo -e "\nDone."
