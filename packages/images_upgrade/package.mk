78fd41b398f868d0acfe94743257874d058666dc
nick <nick@khadas.com>
2018-06-15 16:51:07 +0800

    packages: images_upgrade: bump to 4cad389
    add Rockchip image packing tools

--- packages/images_upgrade/package.mk
+++ packages/images_upgrade/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="images_upgrade"
-PKG_VERSION="7a276d5"
-PKG_SHA256="fc0db1a8c154605f2e2f5c5d1862b23102fb5dd3ce01db6243462dfb0ec99e4a"
+PKG_VERSION="4cad389"
+PKG_SHA256="d0284ac3a928ebd8a2f5c4ef3de53a2343c73c39cc308bbc981648c71d02e5b0"
 PKG_SOURCE_DIR="images_upgrade-${PKG_VERSION}*"
 PKG_SITE="https://github.com/khadas/images_upgrade"
 PKG_URL="https://github.com/khadas/images_upgrade/archive/$PKG_VERSION.tar.gz"
