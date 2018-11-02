a3ea1151a92832f93298fb87950213ef0cfd8919
Nick <nick@khadas.com>
2018-11-02 18:17:24 +0800

    packages: rkbin: bump to 0a6c7c6
    fixup some boards power on block at trust

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="rkbin"
-PKG_VERSION="b789f812b52931534b7b6cf762428f94e3942bec"
-PKG_SHA256="f900ad5ffbbfa946f16b7ea6e0b8671b621f13c38331da9c51c6ca9486f18dde"
+PKG_VERSION="0a6c7c6966a60093a395d094f05dbc5facc6df06"
+PKG_SHA256="2980d3bfeb48e46058b74f1245df22e589926abe5db8cb23d195a23fdd4f6341"
 PKG_SOURCE_DIR="rkbin-${PKG_VERSION}*"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
 PKG_URL="https://github.com/rockchip-linux/rkbin/archive/$PKG_VERSION.tar.gz"
@@ -14,7 +14,7 @@ PKG_NEED_BUILD="NO"
 make_host() {
 	chmod +x firmware_merger
 	# Fixup links
-	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.14.bin rk3399_ddr.bin
+	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.15.bin rk3399_ddr.bin
 	ln -fs bin/rk33/rk3399_miniloader_spinor_v1.14.bin rk3399miniloaderall_spi.bin
 	ln -fs bin/rk33/rk3399_miniloader_v1.15.bin rk3399miniloaderall.bin
 }
