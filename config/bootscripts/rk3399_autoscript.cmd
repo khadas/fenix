4c525fc467477b917b51d89943a387a0c0135ca8
Nick <nick@khadas.com>
2018-11-07 11:06:44 +0800

    Edge: add basic support for mainline linux

--- config/bootscripts/rk3399_autoscript.cmd
+++ config/bootscripts/rk3399_autoscript.cmd
@@ -2,18 +2,11 @@ echo "Run Khadas boot script"
 
 kbi boarddetect
 
-if test ${board_type} = 1; then
-	setenv boot_dtb "rk3399-khadas-edge-linux.dtb";
-else if test ${board_type} = 2; then
-	setenv boot_dtb "rk3399-khadas-edgev-linux.dtb";
-else if test ${board_type} = 3; then
-	setenv boot_dtb "rk3399-khadas-captain-linux.dtb";
-else if test ${board_type} = 0; then
-	setenv boot_dtb;
+if test ${board_type} = 0; then
 	echo "Unsupported board detected! Stop here. Reboot...";
 	sleep 5;
 	reset;
-fi;fi;fi;fi
+fi
 
 setenv emmc_root_part 7
 setenv emmc_boot_part 7
@@ -22,17 +15,54 @@ setenv sd_boot_part   1
 
 if test ${devnum} = 0; then
 	echo "Uboot loaded from eMMC.";
-	setenv boot_dtb "/boot/dtb/${boot_dtb}";
 	setenv boot_env_part ${emmc_boot_part};
+	setenv mark_prefix "boot/"
 else if test ${devnum} = 1; then
 	echo "Uboot loaded from SD.";
-	setenv boot_dtb "/dtb/${boot_dtb}";
 	setenv boot_env_part ${sd_boot_part};
+	setenv mark_prefix ""
 fi;fi;
 
-echo boot_dtb: ${boot_dtb}
+if test -e mmc ${devnum}:${boot_env_part} ${mark_prefix}.next; then
+	if test ${board_type} = 1; then
+		setenv boot_dtb "rk3399-khadas-edge.dtb";
+	else if test ${board_type} = 2; then
+		setenv boot_dtb "rk3399-khadas-edgev.dtb";
+	else if test ${board_type} = 3; then
+		setenv boot_dtb "rk3399-khadas-captain.dtb";
+	fi;fi;fi
+else
+	if test ${board_type} = 1; then
+		setenv boot_dtb "rk3399-khadas-edge-linux.dtb";
+	else if test ${board_type} = 2; then
+		setenv boot_dtb "rk3399-khadas-edgev-linux.dtb";
+	else if test ${board_type} = 3; then
+		setenv boot_dtb "rk3399-khadas-captain-linux.dtb";
+	fi;fi;fi
+fi
 
-setenv condev "earlyprintk console=ttyFIQ0,1500000n8 console=tty0"
+if test ${devnum} = 0; then
+	if test -e mmc ${devnum}:${boot_env_part} ${mark_prefix}.next; then
+		setenv dtb_prefix "/boot/dtb/rockchip/";
+	else
+		setenv dtb_prefix "/boot/dtb/";
+	fi
+else if test ${devnum} = 1; then
+	if test -e mmc ${devnum}:${boot_env_part} ${mark_prefix}.next; then
+		setenv dtb_prefix "/dtb/rockchip/";
+	else
+		setenv dtb_prefix "/dtb/";
+	fi
+fi;fi;
+
+echo DTB: ${dtb_prefix}${boot_dtb}
+
+if test -e mmc ${devnum}:${boot_env_part} ${mark_prefix}.next; then
+	setenv condev "earlyprintk console=ttyS2,1500000n8 console=tty0"
+else
+	setenv condev "earlyprintk console=ttyFIQ0,1500000n8 console=tty0"
+fi
+
 setenv boot_start booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
 
 # Import environment from env.txt
@@ -48,7 +78,7 @@ for distro_bootpart in ${devplist}; do
 
 	if load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} uInitrd; then
 		if load ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} zImage; then
-			if load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} ${boot_dtb}; then
+			if load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} ${dtb_prefix}${boot_dtb}; then
 				run boot_start;
 			fi;
 		fi;
