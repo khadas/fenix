05070ff0f5c53527a7ccf1f1e6af05a52f772bf1
Nick <nick@khadas.com>
2019-01-16 16:29:55 +0800

    packages: rkbin: bump to bf66bc3
    fixup LPDDR4 issue

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="rkbin"
-PKG_VERSION="0a6c7c6966a60093a395d094f05dbc5facc6df06"
-PKG_SHA256="2980d3bfeb48e46058b74f1245df22e589926abe5db8cb23d195a23fdd4f6341"
+PKG_VERSION="1f782f6ac8a08e4dde2867998d155f455d93e8d4"
+PKG_SHA256="e9791d22a703ca8bda61b3bcfa2026c0cc685596e8023d8444f966810145146f"
 PKG_SOURCE_DIR="rkbin-${PKG_VERSION}*"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
 PKG_URL="https://github.com/rockchip-linux/rkbin/archive/$PKG_VERSION.tar.gz"
@@ -14,7 +14,7 @@ PKG_NEED_BUILD="NO"
 make_host() {
 	chmod +x firmware_merger
 	# Fixup links
-	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.15.bin rk3399_ddr.bin
+	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.17.bin rk3399_ddr.bin
 	ln -fs bin/rk33/rk3399_miniloader_spinor_v1.14.bin rk3399miniloaderall_spi.bin
 	ln -fs bin/rk33/rk3399_miniloader_v1.15.bin rk3399miniloaderall.bin
 }
