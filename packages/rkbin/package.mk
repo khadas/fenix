2fecfa0fa2c437898736b7336f76557054bd6327
Nick <nick@khadas.com>
2019-03-01 15:27:52 +0800

    packages: rkbin: bump to 8a31ba3

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="rkbin"
-PKG_VERSION="8a31ba3a305601e936312a0502a41b11f20379fb"
-PKG_SHA256="7bbe9da84dbd39b0a26e56888b5dd794712a813688cbae5c54b771631abf3804"
+PKG_VERSION="9b8def1b3dc98937bcdff9a4a826cece46e02901"
+PKG_SHA256="914a9b15f4384ba2231337284d2760f6906126b3fca63405bf8889170fdcdf32"
 PKG_SOURCE_DIR="rkbin-${PKG_VERSION}*"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
 PKG_URL="https://github.com/rockchip-linux/rkbin/archive/$PKG_VERSION.tar.gz"
@@ -16,6 +16,6 @@ make_host() {
 	# Fixup links
 	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.18.bin rk3399_ddr.bin
 	ln -fs bin/rk33/rk3399_miniloader_spinor_v1.14.bin rk3399miniloaderall_spi.bin
-	ln -fs bin/rk33/rk3399_miniloader_v1.15.bin rk3399miniloaderall.bin
+	ln -fs bin/rk33/rk3399_miniloader_v1.18.bin rk3399miniloaderall.bin
 }
 
