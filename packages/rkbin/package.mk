21853ffb25b8e9714ded82d14c2307ac56fb430f
Nick <nick@khadas.com>
2019-03-26 14:36:34 +0800

    packages: rkbin: bump to 9fc33ae

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="rkbin"
-PKG_VERSION="9b8def1b3dc98937bcdff9a4a826cece46e02901"
-PKG_SHA256="914a9b15f4384ba2231337284d2760f6906126b3fca63405bf8889170fdcdf32"
+PKG_VERSION="9fc33aee92908b538ca6687550be437415efae8e"
+PKG_SHA256="f20a2fb913b39854dba0e26a88945730ba95ba264b185c7fe1b9617fff2338c8"
 PKG_SOURCE_DIR="rkbin-${PKG_VERSION}*"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
 PKG_URL="https://github.com/rockchip-linux/rkbin/archive/$PKG_VERSION.tar.gz"
@@ -14,8 +14,8 @@ PKG_NEED_BUILD="NO"
 make_host() {
 	chmod +x firmware_merger
 	# Fixup links
-	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.18.bin rk3399_ddr.bin
+	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.19.bin rk3399_ddr.bin
 	ln -fs bin/rk33/rk3399_miniloader_spinor_v1.14.bin rk3399miniloaderall_spi.bin
-	ln -fs bin/rk33/rk3399_miniloader_v1.18.bin rk3399miniloaderall.bin
+	ln -fs bin/rk33/rk3399_miniloader_v1.19.bin rk3399miniloaderall.bin
 }
 
