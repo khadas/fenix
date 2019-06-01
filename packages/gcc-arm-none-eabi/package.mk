05b4b5ba4143e4c41b0b1eaf1f01e2d3abbcd0bc
Nick <nick@khadas.com>
2019-06-01 17:33:30 +0800

    toolchain: add gcc-arm-none-eabi
    Signed-off-by: Nick <nick@khadas.com>

---
+++ packages/gcc-arm-none-eabi/package.mk
@@ -0,0 +1,19 @@
+PKG_NAME="gcc-arm-none-eabi"
+PKG_VERSION="6-2017-q2-update"
+PKG_VERSION_SHORT="6-2017q2"
+PKG_SHA256=""
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/${PKG_VERSION_SHORT}/gcc-arm-none-eabi-${PKG_VERSION}-linux.tar.bz2"
+PKG_SOURCE_DIR="gcc-arm-none-eabi-${PKG_VERSION}-linux"
+PKG_SOURCE_NAME="$(basename $PKG_URL)"
+PKG_NEED_BUILD="NO"
+PKG_SHORTDESC="GCC for building Amlogic U-Boot firmware"
+
+makeinstall_host() {
+	mkdir -p $TOOLCHAINS/gcc-arm-none-eabi/
+	rm -rf $TOOLCHAINS/gcc-arm-none-eabi/*
+	cp -a $BUILD/$PKG_NAME-$PKG_VERSION/* $TOOLCHAINS/gcc-arm-none-eabi
+}
