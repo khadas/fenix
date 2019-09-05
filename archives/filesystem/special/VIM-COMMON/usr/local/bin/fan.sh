caed0c4bc4fbbb1694e859918744ed35d558bfa4
Giovanni Minniti <minniti@de.ibm.com>
2019-09-05 08:17:52 +0200

    Added help, temp, trigger and state commands...

--- archives/filesystem/special/VIM-COMMON/usr/local/bin/fan.sh
+++ archives/filesystem/special/VIM-COMMON/usr/local/bin/fan.sh
@@ -6,6 +6,7 @@ FAN_INPUT="${1:-"boot"}"
 FAN_MODE_NODE="/sys/class/fan/mode"
 FAN_LEVEL_NODE="/sys/class/fan/level"
 FAN_ENABLE_NODE="/sys/class/fan/enable"
+FAN_TEMP_NODE="/sys/class/fan/temp"
 
 AUTO_MODE=1
 MANUAL_MODE=0
@@ -16,8 +17,11 @@ LEVEL_HIGH=3
 
 usage() {
 	echo ""
-	echo "Usage: $0 [on|low|mid|high|auto|off] - Set fan mode/level"
-	echo "       $0 [mode] - Query fan mode/level"
+	echo "Usage: $0 [on|low|mid|high|auto|off] :: Set fan mode/level"
+	echo "       $0 [temp]    :: Query cpu temperature"
+	echo "       $0 [trig]    :: Query fan trigger temperature"
+	echo "       $0 [mode]    :: Query fan mode/level"
+	echo "       $0 --help|-h :: This text"
 	echo ""
 	echo "Examp: $0 auto"
 }
@@ -27,7 +31,7 @@ message() {
 	local TXT="$2"
 	local RC="$3"
 	echo "$TYPE $TXT"
-	if [ "$TYPE" = "Error:" ]; then usage; fi
+	if [ "$TYPE" = "Error:" ] || [ "$TYPE" = "Help:" ]; then usage; fi
 	logger -s "$TXT" "$RC" 1>/dev/null 2>&1
 	exit $RC
 }
@@ -44,14 +48,28 @@ if [ $(id -u) != 0 ]; then
    message "Error:" "Use sudo or run with root privilege!" 1
 fi
 
-if [ "$FAN_INPUT" = "mode" ]; then
+if [ "$FAN_INPUT" = "temp" ]; then
+	mode=$FAN_INPUT
+	FAN_TEMP=$(cat $FAN_TEMP_NODE | grep cpu_${FAN_INPUT} | cut -d':' -f2)
+elif [ "$FAN_INPUT" = "trig" ]; then
+	mode=$FAN_INPUT
+	FAN_LVL_0=$(sudo cat /sys/class/fan/temp|grep level | awk '{print $4}' | cut -d':' -f2)
+	FAN_LVL_1=$(sudo cat /sys/class/fan/temp|grep level | awk '{print $5}' | cut -d':' -f2)
+	FAN_LVL_2=$(sudo cat /sys/class/fan/temp|grep level | awk '{print $6}' | cut -d':' -f2)
+elif [ "$FAN_INPUT" = "mode" ]; then
 	mode=$FAN_INPUT
 
 	FAN_MODE=$(cat $FAN_MODE_NODE | awk '{print $3}')
 	FAN_LEVEL=$(cat $FAN_LEVEL_NODE | awk '{print $3}')
+	FAN_STATE=$(cat $FAN_ENABLE_NODE | awk '{print $3}')
 	
 	if [ $FAN_MODE -eq 0 ]; then FAN_MODE=manual; else FAN_MODE=auto; fi
 	
+	case $FAN_STATE in
+		0) FAN_STATE=inactive; mode=mode-off; ;;
+		1) FAN_STATE=active ;;
+	esac
+
 	case $FAN_LEVEL in
 		0) FAN_LEVEL=off ;;
 		1) FAN_LEVEL=low ;;
@@ -93,12 +111,27 @@ case $mode in
 		echo $AUTO_MODE > $FAN_MODE_NODE
 		echo 1 > $FAN_ENABLE_NODE
 		;;
+	mode-off)
+		echo "Fan state: $FAN_STATE"
+		;;
 	mode)
 		echo "Fan mode: $FAN_MODE"
 		echo "Fan level: $FAN_LEVEL"
-		;;	
+		echo "Fan state: $FAN_STATE"
+		;;
+	temp)
+		echo "Fan temp: $FAN_TEMP"
+		;;
+	trig)
+		echo "Fan trigger low temp: $FAN_LVL_0"
+		echo "Fan trigger mid temp: $FAN_LVL_1"
+		echo "Fan trigger high temp: $FAN_LVL_2"
+		;;
+	--help|-h) 
+		message "Help:" "Command line parameters:" 0
+		;;
 	*)
-		message "Error:" "Fan mode/level is unknown!" 1
+		message "Error:" "Fan mode/level is unknown!" 2
 		;;
 esac
 
