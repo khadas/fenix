5e7c80663c0452975fe12bae655851b5defa3c94
hyphop <email2tema@gmail.com>
2019-09-18 04:31:14 +0300

    small fix2

--- packages/gcc-linaro-aarch64-linux-gnu-6.3/package.mk
+++ packages/gcc-linaro-aarch64-linux-gnu-6.3/package.mk
@@ -17,6 +17,6 @@ makeinstall_host() {
 #	rm -rf $TOOLCHAINS/gcc-linaro-aarch64-linux-gnu-6.3/*
 #	cp -a $BUILD/$PKG_NAME-$PKG_VERSION/* $TOOLCHAINS/gcc-linaro-aarch64-linux-gnu-6.3
 	[ -d "$TOOLCHAINS" ] || mkdir -p "$TOOLCHAINS"
-	[ -d "$TOOLCHAINS/$PKG_NAME" ] && rm -rd "$TOOLCHAINS/$PKG_NAME"
+	[ -d "$TOOLCHAINS/$PKG_NAME" ] && rm -rdf "$TOOLCHAINS/$PKG_NAME"
 	ln -sf "../$PKG_NAME-$PKG_VERSION" "$TOOLCHAINS/$PKG_NAME"
 }
