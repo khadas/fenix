f58e75d58dd9d256b5dc9f9ef0c93e24581e9e15
Nick Xie <nick@khadas.com>
2019-10-24 13:33:39 +0800

    packages: wiringpi_debs: bump to b30f87b
    Signed-off-by: Nick Xie <nick@khadas.com>

--- packages/wiringpi_debs/package.mk
+++ packages/wiringpi_debs/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="wiringpi_debs"
-PKG_VERSION="030c2bec94aa43661c5f0a08b4ff79645f206614"
-PKG_SHA256="147a1cb194651ecbd3d51cad82c70e27df65a082c09d10194134e5139ed521eb"
+PKG_VERSION="b30f87b91d2228f582a164e8dd39a2d710d0415d"
+PKG_SHA256="4faa4d8f164f77b89e782a824e70ea665679c6b9a7bc3b26654247eb672e8390"
 PKG_SOURCE_DIR="wiringpi_debs-${PKG_VERSION}*"
 PKG_SITE="https://github.com/numbqq/wiringpi_debs"
 PKG_URL="https://github.com/numbqq/wiringpi_debs/archive/$PKG_VERSION.tar.gz"
