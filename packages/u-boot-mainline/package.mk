86c4b6a7fee8e82410f72c73df912b2ef9e90941
Nick Xie <nick@khadas.com>
2019-12-18 15:34:48 +0800

    VIM3: add mainline u-boot support
    Signed-off-by: Nick Xie <nick@khadas.com>

--- packages/u-boot-mainline/package.mk
+++ packages/u-boot-mainline/package.mk
@@ -25,14 +25,40 @@ post_make_target() {
 		rm -rf $BUILD/$PKG_NAME-$PKG_VERSION/fip
 		cp -r $PKGS_DIR/$PKG_NAME/fip/$KHADAS_BOARD $BUILD/$PKG_NAME-$PKG_VERSION/fip
 		cp u-boot.bin fip/bl33.bin
-		fip/blx_fix.sh fip/bl30.bin fip/zero_tmp fip/bl30_zero.bin fip/bl301.bin fip/bl301_zero.bin fip/bl30_new.bin bl30
-		python fip/acs_tool.pyc fip/bl2.bin fip/bl2_acs.bin fip/acs.bin 0
-		fip/blx_fix.sh fip/bl2_acs.bin fip/zero_tmp fip/bl2_zero.bin fip/bl21.bin fip/bl21_zero.bin fip/bl2_new.bin bl2
-		fip/aml_encrypt_gxl --bl3enc --input fip/bl30_new.bin
-		fip/aml_encrypt_gxl --bl3enc --input fip/bl31.img
-		fip/aml_encrypt_gxl --bl3enc --input fip/bl33.bin
-		fip/aml_encrypt_gxl --bl2sig --input fip/bl2_new.bin --output fip/bl2.n.bin.sig
-		fip/aml_encrypt_gxl --bootmk --output fip/u-boot.bin --bl2 fip/bl2.n.bin.sig --bl30 fip/bl30_new.bin.enc --bl31 fip/bl31.img.enc --bl33 fip/bl33.bin.enc
+		if [[ "$KHADAS_BOARD" =~ VIM[12] ]]; then
+			fip/blx_fix.sh fip/bl30.bin fip/zero_tmp fip/bl30_zero.bin fip/bl301.bin fip/bl301_zero.bin fip/bl30_new.bin bl30
+			python fip/acs_tool.pyc fip/bl2.bin fip/bl2_acs.bin fip/acs.bin 0
+			fip/blx_fix.sh fip/bl2_acs.bin fip/zero_tmp fip/bl2_zero.bin fip/bl21.bin fip/bl21_zero.bin fip/bl2_new.bin bl2
+			fip/aml_encrypt_gxl --bl3enc --input fip/bl30_new.bin
+			fip/aml_encrypt_gxl --bl3enc --input fip/bl31.img
+			fip/aml_encrypt_gxl --bl3enc --input fip/bl33.bin
+			fip/aml_encrypt_gxl --bl2sig --input fip/bl2_new.bin --output fip/bl2.n.bin.sig
+			fip/aml_encrypt_gxl --bootmk --output fip/u-boot.bin --bl2 fip/bl2.n.bin.sig --bl30 fip/bl30_new.bin.enc --bl31 fip/bl31.img.enc --bl33 fip/bl33.bin.enc
+		elif [[ "$KHADAS_BOARD" == VIM3 ]]; then
+			fip/blx_fix.sh fip/bl30.bin fip/zero_tmp fip/bl30_zero.bin fip/bl301.bin fip/bl301_zero.bin fip/bl30_new.bin bl30
+			fip/blx_fix.sh fip/bl2.bin fip/zero_tmp fip/bl2_zero.bin fip/acs.bin fip/bl21_zero.bin fip/bl2_new.bin bl2
+			fip/aml_encrypt_g12b --bl30sig --input fip/bl30_new.bin --output fip/bl30_new.bin.g12a.enc --level v3
+			fip/aml_encrypt_g12b --bl3sig --input fip/bl30_new.bin.g12a.enc --output fip/bl30_new.bin.enc --level v3 --type bl30
+			fip/aml_encrypt_g12b --bl3sig --input fip/bl31.img --output fip/bl31.img.enc --level v3 --type bl31
+			fip/aml_encrypt_g12b --bl3sig --input fip/bl33.bin --compress lz4 --output fip/bl33.bin.enc --level v3 --type bl33 --compress lz4
+			fip/aml_encrypt_g12b --bl2sig --input fip/bl2_new.bin --output fip/bl2.n.bin.sig
+			fip/aml_encrypt_g12b --bootmk \
+				--output fip/u-boot.bin \
+				--bl2 fip/bl2.n.bin.sig \
+				--bl30 fip/bl30_new.bin.enc \
+				--bl31 fip/bl31.img.enc \
+				--bl33 fip/bl33.bin.enc \
+				--ddrfw1 fip/ddr4_1d.fw \
+				--ddrfw2 fip/ddr4_2d.fw \
+				--ddrfw3 fip/ddr3_1d.fw \
+				--ddrfw4 fip/piei.fw \
+				--ddrfw5 fip/lpddr4_1d.fw \
+				--ddrfw6 fip/lpddr4_2d.fw \
+				--ddrfw7 fip/diag_lpddr4.fw \
+				--ddrfw8 fip/aml_ddr.fw \
+				--ddrfw9 fip/lpddr3_1d.fw \
+				--level v3
+		fi
 	elif [ "$VENDOR" == "Rockchip" ]; then
 		if [[ $(type -t uboot_custom_postprocess) == function ]]; then
 			uboot_custom_postprocess
