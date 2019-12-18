ae0fe0dbbf90f3f7a99bfbbf0b7f23189fe153a7
Nick Xie <nick@khadas.com>
2019-12-18 15:57:57 +0800

    VIM3L: add mainline u-boot support
    Signed-off-by: Nick Xie <nick@khadas.com>

--- packages/u-boot-mainline/package.mk
+++ packages/u-boot-mainline/package.mk
@@ -34,15 +34,20 @@ post_make_target() {
 			fip/aml_encrypt_gxl --bl3enc --input fip/bl33.bin
 			fip/aml_encrypt_gxl --bl2sig --input fip/bl2_new.bin --output fip/bl2.n.bin.sig
 			fip/aml_encrypt_gxl --bootmk --output fip/u-boot.bin --bl2 fip/bl2.n.bin.sig --bl30 fip/bl30_new.bin.enc --bl31 fip/bl31.img.enc --bl33 fip/bl33.bin.enc
-		elif [[ "$KHADAS_BOARD" == VIM3 ]]; then
+		elif [[ "$KHADAS_BOARD" =~ VIM[3*] ]]; then
+			if [ "$KHADAS_BOARD" == "VIM3" ]; then
+				aml_encrypt=aml_encrypt_g12b
+			elif [ "$KHADAS_BOARD" == "VIM3L" ]; then
+				aml_encrypt=aml_encrypt_g12a
+			fi
 			fip/blx_fix.sh fip/bl30.bin fip/zero_tmp fip/bl30_zero.bin fip/bl301.bin fip/bl301_zero.bin fip/bl30_new.bin bl30
 			fip/blx_fix.sh fip/bl2.bin fip/zero_tmp fip/bl2_zero.bin fip/acs.bin fip/bl21_zero.bin fip/bl2_new.bin bl2
-			fip/aml_encrypt_g12b --bl30sig --input fip/bl30_new.bin --output fip/bl30_new.bin.g12a.enc --level v3
-			fip/aml_encrypt_g12b --bl3sig --input fip/bl30_new.bin.g12a.enc --output fip/bl30_new.bin.enc --level v3 --type bl30
-			fip/aml_encrypt_g12b --bl3sig --input fip/bl31.img --output fip/bl31.img.enc --level v3 --type bl31
-			fip/aml_encrypt_g12b --bl3sig --input fip/bl33.bin --compress lz4 --output fip/bl33.bin.enc --level v3 --type bl33 --compress lz4
-			fip/aml_encrypt_g12b --bl2sig --input fip/bl2_new.bin --output fip/bl2.n.bin.sig
-			fip/aml_encrypt_g12b --bootmk \
+			fip/${aml_encrypt} --bl30sig --input fip/bl30_new.bin --output fip/bl30_new.bin.g12a.enc --level v3
+			fip/${aml_encrypt} --bl3sig --input fip/bl30_new.bin.g12a.enc --output fip/bl30_new.bin.enc --level v3 --type bl30
+			fip/${aml_encrypt} --bl3sig --input fip/bl31.img --output fip/bl31.img.enc --level v3 --type bl31
+			fip/${aml_encrypt} --bl3sig --input fip/bl33.bin --compress lz4 --output fip/bl33.bin.enc --level v3 --type bl33 --compress lz4
+			fip/${aml_encrypt} --bl2sig --input fip/bl2_new.bin --output fip/bl2.n.bin.sig
+			fip/${aml_encrypt} --bootmk \
 				--output fip/u-boot.bin \
 				--bl2 fip/bl2.n.bin.sig \
 				--bl30 fip/bl30_new.bin.enc \
