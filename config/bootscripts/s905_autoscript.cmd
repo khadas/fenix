8d5d29268af8d029c64704c8b4fc6bc265c08b39
Nick Xie <nick@khadas.com>
2019-12-21 12:18:08 +0800

    bootscript: VIMs: update for mainline uboot/linux
    Signed-off-by: Nick Xie <nick@khadas.com>

--- config/bootscripts/s905_autoscript.cmd
+++ config/bootscripts/s905_autoscript.cmd
@@ -5,8 +5,6 @@ setenv dtb_loadaddr "0x1000000"
 setenv initrd_loadaddr "0x13000000"
 setenv env_loadaddr "0x20000000"
 
-setenv hdmiargs "logo=${display_layer},loaded,${fb_addr},${outputmode} vout=${outputmode},enable"
-
 # Detect hardware version
 kbi init
 kbi hwver
@@ -43,6 +41,18 @@ else
 fi;fi;fi;fi;
 
 echo "uboot type: $uboottype"
+
+if test "X${uboottype}" = "Xmainline"; then
+	setenv hdmiargs "";
+	setenv ddr "";
+	setenv wol "";
+	setenv rebootmode "";
+else
+	setenv hdmiargs "logo=${display_layer},loaded,${fb_addr},${outputmode} vout=${outputmode},enable"
+	setenv ddr "ddr_size=${ddr_size}";
+	setenv wol "wol_enable=${wol_enable}";
+	setenv rebootmode "reboot_mode=${reboot_mode}"
+fi;
 
 if test "X${autoscript_source}" = "Xmmc"; then
 	echo "autoscript loaded from: mmc";
@@ -120,7 +130,7 @@ for dev in ${devs}; do
 						if test "X${eth_mac}" = "X"; then
 							echo "Set default mac address to ethaddr: ${ethaddr}!";
 							setenv eth_mac ${ethaddr};
-							setenv save_ethmac "yes";
+							setenv saveethmac "save_ethmac=yes";
 						fi;
 						if test -e ${dev} ${dev_num}:${distro_bootpart} ${mark_prefix}.next; then
 							echo "Booting mainline kernel...";
@@ -128,44 +138,49 @@ for dev in ${devs}; do
 						else
 							echo "Booting legacy kernel...";
 							setenv condev "console=ttyS0,115200n8 console=tty0 no_console_suspend consoleblank=0";
+
+							# Setup dtb for different HW version
+							fdt addr ${dtb_loadaddr};
+							fdt resize 65536;
+							if test "X${hwver}" = "XVIM1.V14"; then
+								fdt set /soc/cbus@c1100000/i2c@87c0/khadas-mcu hwver "VIM1.V14";
+							else if test "X${hwver}" = "XVIM2.V14"; then
+								fdt set /fan hwver "VIM2.V14";
+								fdt set /i2c@c11087c0/khadas-mcu hwver "VIM2.V14";
+								fdt set /soc/cbus@c1100000/i2c@87c0/khadas-mcu hwver "VIM2.V14";
+							else if test "X${hwver}" = "XVIM3.V11" || test "X${hwver}" = "XVIM3.V12"; then
+								fdt set /soc/aobus@ff800000/i2c@5000/khadas-mcu hwver ${hwver};
+								kbi init;
+								kbi portmode r;
+								if test ${port_mode} = 0; then
+									fdt set /usb3phy@ffe09080 portnum <1>;
+									fdt set /pcieA@fc000000 status disabled;
+								else
+									fdt set /usb3phy@ffe09080 portnum <0>;
+									fdt set /pcieA@fc000000 status okay;
+								fi;
+							fi;fi;fi;
 						fi;
 
-						fdt addr ${dtb_loadaddr};
-						fdt resize 65536;
-						if test "X${hwver}" = "XVIM1.V14"; then
-							fdt set /soc/cbus@c1100000/i2c@87c0/khadas-mcu hwver "VIM1.V14";
-						else if test "X${hwver}" = "XVIM2.V14"; then
-							fdt set /fan hwver "VIM2.V14";
-							fdt set /i2c@c11087c0/khadas-mcu hwver "VIM2.V14";
-							fdt set /soc/cbus@c1100000/i2c@87c0/khadas-mcu hwver "VIM2.V14";
-						else if test "X${hwver}" = "XVIM3.V11" || test "X${hwver}" = "XVIM3.V12"; then
-							fdt set /soc/aobus@ff800000/i2c@5000/khadas-mcu hwver ${hwver};
-							kbi init;
-							kbi portmode r;
-							if test ${port_mode} = 0; then
-								fdt set /usb3phy@ffe09080 portnum <1>;
-								fdt set /pcieA@fc000000 status disabled;
-							else
-								fdt set /usb3phy@ffe09080 portnum <0>;
-								fdt set /pcieA@fc000000 status okay;
-							fi;
-						fi;fi;fi;
 						if test "X${imagetype}" = "XEMMC_MBR"; then
 							echo "Remove eMMC vendor partitions...";
 							fdt rm /partitions;
 						fi;
-						if test "X${hdmi_autodetect}" != "Xyes"; then
-							if test "X${hdmi}" = "X"; then
-								echo "HDMI: 'hdmi' value is missing, set to default value 720p60hz!";
-								setenv hdmi 720p60hz;
+
+						if test "X${uboottype}" != "Xmainline"; then
+							if test "X${hdmi_autodetect}" != "Xyes"; then
+								if test "X${hdmi}" = "X"; then
+									echo "HDMI: 'hdmi' value is missing, set to default value 720p60hz!";
+									setenv hdmi 720p60hz;
+								fi;
+								echo "HDMI: Custom mode: ${hdmi}";
+								setenv hdmiargs "${hdmiargs} hdmimode=${hdmi}";
+							else
+								echo "HDMI: Autodetect: ${hdmimode}";
+								setenv hdmiargs "${hdmiargs} hdmimode=${hdmimode}";
 							fi;
-							echo "HDMI: Custom mode: ${hdmi}";
-							setenv hdmiargs "${hdmiargs} hdmimode=${hdmi}";
-						else
-							echo "HDMI: Autodetect: ${hdmimode}";
-							setenv hdmiargs "${hdmiargs} hdmimode=${hdmimode}";
 						fi;
-						setenv bootargs "root=${rootdev} rootfstype=ext4 rootflags=data=writeback rw ubootpart=${ubootpartuuid} ${condev} ${hdmiargs} ${panelargs} fsck.repair=yes net.ifnames=0 ddr_size=${ddr_size} wol_enable=${wol_enable}  jtag=disable mac=${eth_mac} androidboot.mac=${eth_mac} save_ethmac=${save_ethmac} fan=${fan_mode} khadas_board=${khadas_board} hwver=${hwver} coherent_pool=${dma_size} reboot_mode=${reboot_mode} imagetype=${imagetype} uboottype=${uboottype}";
+						setenv bootargs "root=${rootdev} rootfstype=ext4 rootflags=data=writeback rw ubootpart=${ubootpartuuid} ${condev} ${hdmiargs} ${panelargs} fsck.repair=yes net.ifnames=0 ${ddr} ${wol} jtag=disable mac=${eth_mac} androidboot.mac=${eth_mac} ${saveethmac} fan=${fan_mode} khadas_board=${khadas_board} hwver=${hwver} coherent_pool=${dma_size} ${rebootmode} imagetype=${imagetype} uboottype=${uboottype}";
 						run boot_start;
 					fi;
 				fi;
