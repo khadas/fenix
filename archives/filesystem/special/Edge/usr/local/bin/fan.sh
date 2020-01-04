828a7e59f72ad91590c455aa568db11c1323aa23
Nick Xie <nick@khadas.com>
2020-01-04 11:35:51 +0800

    Edge: update FAN control script
    Signed-off-by: Nick Xie <nick@khadas.com>

--- archives/filesystem/special/Edge/usr/local/bin/fan.sh
+++ archives/filesystem/special/Edge/usr/local/bin/fan.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-FAN_LEVEL_NODE="/sys/class/thermal/cooling_device0/cur_state"
-TEMP_NODE="/sys/class/thermal/thermal_zone0/temp"
+RC=0
+FAN_INPUT="${1:-"boot"}"
 
-TEMP_LOW=50000
-TEMP_MID=60000
-TEMP_HIGH=70000
+FAN_MODE_NODE="/sys/class/hwmon/hwmon0/mode"
+FAN_LEVEL_NODE="/sys/class/thermal/cooling_device0/cur_state"
+FAN_ENABLE_NODE="/sys/class/hwmon/hwmon0/enable"
+FAN_TEMP_NODE="/sys/class/thermal/thermal_zone0/temp"
 
 AUTO_MODE=1
 MANUAL_MODE=0
 
-FAN_LEVEL_OFF=0
-FAN_LEVEL_LOW=1
-FAN_LEVEL_MID=2
-FAN_LEVEL_HIGH=3
+LEVEL_LOW=1
+LEVEL_MID=2
+LEVEL_HIGH=3
 
-SLEEP_TIME=10
+usage() {
+	echo ""
+	echo "Usage: $0 [on|auto|off]  :: Set fan mode"
+	echo "       $0 [low|mid|high] :: Set fan level"
+	echo "       $0 [temp]         :: Query cpu temperature"
+	echo "       $0 [mode]         :: Query fan mode/level"
+	echo "       $0 [--help|-h]    :: This text"
+	echo ""
+	echo "Examp: $0 auto"
+}
+
+message() {
+	local TYPE="$1"
+	local TXT="$2"
+	local RC="$3"
+	echo "$TYPE $TXT"
+	if [ "$TYPE" = "Error:" ] || [ "$TYPE" = "Help:" ]; then usage; fi
+	logger -s "$TXT" "$RC" 1>/dev/null 2>&1
+	exit $RC
+}
 
 # off
 # auto
@@ -24,70 +43,86 @@ SLEEP_TIME=10
 # high
 mode="auto"
 
+# Require root privilege
+#if [ $(id -u) != 0 ]; then
+#   message "Error:" "Use sudo or run with root privilege!" 1
+#fi
 
+if [ "$FAN_INPUT" = "temp" ]; then
+	mode=$FAN_INPUT
+	FAN_TEMP=$(cat $FAN_TEMP_NODE)
+elif [ "$FAN_INPUT" = "mode" ]; then
+	mode=$FAN_INPUT
 
-for m in $(cat /proc/cmdline); do
-	case ${m} in
-		fan=*) mode=${m#*=} ;;
+	FAN_MODE=$(cat $FAN_MODE_NODE | awk '{print $3}')
+	FAN_LEVEL=$(cat $FAN_LEVEL_NODE)
+	FAN_STATE=$(cat $FAN_ENABLE_NODE | awk '{print $3}')
+
+	if [ $FAN_MODE -eq 0 ]; then FAN_MODE=manual; else FAN_MODE=auto; fi
+
+	case $FAN_STATE in
+		0) FAN_STATE=inactive; mode=mode-off; ;;
+		1) FAN_STATE=active ;;
 	esac
-done
 
-if [ -z $mode ]; then
-	mode="auto"
-fi
-
-echo "FAN mode: $mode"
-
-if [ "$mode" == "auto" ]; then
-	TEMP=0
-	TEMP_NEW=0
-	LEVEL=$FAN_LEVEL_OFF
-	LEVEL_NEW=0
-
-	# Set FAN initial state
-	echo $LEVEL > $FAN_LEVEL_NODE
-
-	while true
-	do
-		sleep $SLEEP_TIME
-		TEMP_NEW=`cat $TEMP_NODE`
-
-		if [ $TEMP_NEW != $TEMP ]; then
-			if [ $TEMP_NEW -ge $TEMP_HIGH ]; then
-				LEVEL_NEW=$FAN_LEVEL_HIGH
-			elif [ $TEMP_NEW -ge $TEMP_MID ]; then
-				LEVEL_NEW=$FAN_LEVEL_MID
-			elif [ $TEMP_NEW -ge $TEMP_LOW ]; then
-				LEVEL_NEW=$FAN_LEVEL_LOW
-			else
-				LEVEL_NEW=$FAN_LEVEL_OFF
-			fi
-
-			if [ "$LEVEL_NEW" != "$LEVEL" ]; then
-				echo $LEVEL_NEW > $FAN_LEVEL_NODE
-				LEVEL=$LEVEL_NEW
-			fi
-
-			TEMP=$TEMP_NEW
-		fi
+	case $FAN_LEVEL in
+		0) FAN_LEVEL=off ;;
+		1) FAN_LEVEL=low ;;
+		2) FAN_LEVEL=mid ;;
+		3) FAN_LEVEL=high ;;
+	esac
+elif [ "$FAN_INPUT" != "boot" ]; then
+	mode=$FAN_INPUT
+else {
+	for m in $(cat /proc/cmdline); do
+		case ${m} in
+			fan=*) mode=${m#*=} ;;
+		esac
 	done
+	}
 fi
 
 case $mode in
 	off)
-		LEVEL=$FAN_LEVEL_OFF
+		echo 0 > $FAN_ENABLE_NODE
+		message "CAUTION:" "Disabling fan can reduce the lifetime of this board!" 0
 		;;
 	low)
-		LEVEL=$FAN_LEVEL_LOW
+		echo 1 > $FAN_ENABLE_NODE
+		echo $MANUAL_MODE > $FAN_MODE_NODE
+		echo $LEVEL_LOW > $FAN_LEVEL_NODE
 		;;
 	mid)
-		LEVEL=$FAN_LEVEL_MID
+		echo 1 > $FAN_ENABLE_NODE
+		echo $MANUAL_MODE > $FAN_MODE_NODE
+		echo $LEVEL_MID > $FAN_LEVEL_NODE
 		;;
 	high)
-		LEVEL=$FAN_LEVEL_HIGH
+		echo 1 > $FAN_ENABLE_NODE
+		echo $MANUAL_MODE > $FAN_MODE_NODE
+		echo $LEVEL_HIGH > $FAN_LEVEL_NODE
+		;;
+	auto|on)
+		echo $AUTO_MODE > $FAN_MODE_NODE
+		echo 1 > $FAN_ENABLE_NODE
+		;;
+	mode-off)
+		echo "Fan state: $FAN_STATE"
+		;;
+	mode)
+		echo "Fan mode: $FAN_MODE"
+		echo "Fan level: $FAN_LEVEL"
+		echo "Fan state: $FAN_STATE"
+		;;
+	temp)
+		echo "Fan temp: $FAN_TEMP"
+		;;
+	--help|-h)
+		message "Help:" "Command line parameters:" 0
+		;;
+	*)
+		message "Error:" "Fan mode/level is unknown!" 2
 		;;
 esac
 
-echo $LEVEL > $FAN_LEVEL_NODE
-
-exit 0
+exit $RC
