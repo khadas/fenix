bf73ff309af1804189c3668c0d885655b2aa283f
Nick Xie <nick@khadas.com>
2021-05-18 17:49:37 +0800

    packages: npu_aml: bump to 86c92d9
    version 6.4.4.3AAA
    
    Signed-off-by: Nick Xie <nick@khadas.com>

--- packages/npu_aml/package.mk
+++ packages/npu_aml/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="npu_aml"
-PKG_VERSION="944fca462866ee3c9c9856534a42e48349e0a2b1"
-PKG_SHA256="e81fa19a48cada3499967c82bdf85a2634e6288bc45d9838719e9ab2e8620b10"
+PKG_VERSION="86c92d9a7cb13b1f7abe70b041b6311fc0980cf5"
+PKG_SHA256="7d3a305ba34c0f4a640af43c2df691fb9a467b3d96a5a14f9b73b375d95c8d46"
 PKG_SOURCE_DIR="npu_aml-${PKG_VERSION}*"
 PKG_SITE="https://github.com/numbqq/npu_aml"
 PKG_URL="https://github.com/numbqq/npu_aml/archive/$PKG_VERSION.tar.gz"
