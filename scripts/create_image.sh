3b19ff03e8a2e15b034337f3566fffe565d6691d
Nick Xie <nick@khadas.com>
2021-06-29 15:55:56 +0800

    config/functions/build: adjust linux debian packages location
    Signed-off-by: Nick Xie <nick@khadas.com>

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -65,11 +65,11 @@ build_debs() {
 		info_msg "Force build kernel debian package..."
 		build_linux_debs
 	else
-		if [[ ! -f $BUILD_DEBS/$VERSION/${LINUX_IMAGE_DEB}_${VERSION}_${DISTRIB_ARCH}.deb ]]; then
+		if [[ ! -f $BUILD_DEBS/$VERSION/$KHADAS_BOARD/${LINUX_IMAGE_DEB}_${VERSION}_${DISTRIB_ARCH}.deb ]]; then
 			build_linux_debs
 		else
 			# Debs exist, but kernel version changed
-			LINUX_DEB_VER=$(dpkg --info $BUILD_DEBS/$VERSION/${LINUX_IMAGE_DEB}_${VERSION}_${DISTRIB_ARCH}.deb | grep Descr | awk '{print $(NF)}') #' coloring bug
+			LINUX_DEB_VER=$(dpkg --info $BUILD_DEBS/$VERSION/$KHADAS_BOARD/${LINUX_IMAGE_DEB}_${VERSION}_${DISTRIB_ARCH}.deb | grep Descr | awk '{print $(NF)}') #' coloring bug
 			LINUX_VER=$(grep "Linux/arm64" $LINUX_DIR/.config | awk '{print $3}')
 			if [ "$LINUX_DEB_VER" != "$LINUX_VER" ]; then
 				build_linux_debs
