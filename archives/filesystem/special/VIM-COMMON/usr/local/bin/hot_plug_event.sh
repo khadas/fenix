26f27d91c759d0441590fbe142422e09edce3117
Jack Zhao <jack.zhao@wesion.com>
2022-03-16 21:06:38 +0800

    VIM4: Fix HDMI best resolution script compatibility
    Signed-off-by: Jack Zhao <jack.zhao@wesion.com>

--- archives/filesystem/special/VIM-COMMON/usr/local/bin/hot_plug_event.sh
+++ archives/filesystem/special/VIM-COMMON/usr/local/bin/hot_plug_event.sh
@@ -2,6 +2,25 @@
 resolutions_v=`edid-decode < /sys/class/amhdmitx/amhdmitx0/rawedid | sed -n '/Detailed mode/,/HorFreq:/p' | sed -n '1,5p' | sed -n '3p'  | awk '{print $1}'`
 resolutions_h=`edid-decode < /sys/class/amhdmitx/amhdmitx0/rawedid | sed -n '/Detailed mode/,/HorFreq:/p' | sed -n '1,5p' | sed -n '2p'  | awk '{print $1}'`
 
+if [ -z "$resolutions_v" -a -z "$resolutions_h" ]
+then
+		resolutions_v=`edid-decode < /sys/class/amhdmitx/amhdmitx0/rawedid | sed -n '/Detailed/,/Display Product Name/p'| sed -n '1,5p' | sed -n '2p' | sed 's/x/ /g' | awk '{print $4}'`
+		resolutions_h=`edid-decode < /sys/class/amhdmitx/amhdmitx0/rawedid | sed -n '/Detailed/,/Display Product Name/p'| sed -n '1,5p' | sed -n '2p' | sed 's/x/ /g' | awk '{print $3}'`
+		hdmi.sh "${resolutions_h}x${resolutions_v}p60hz"
+		if [ $? -eq 0 ]
+		then
+				exit 0
+		else
+				hdmi.sh "${resolutions_v}p60hz" >/dev/null 2>&1
+				if [ $? -ne 0 ]
+				then
+						hdmi.sh 720p60hz
+						exit 0
+				fi
+		fi
+		exit 0
+fi
+
 hdmi.sh "${resolutions_h}x${resolutions_v}p60hz"
 if [ $? -eq 0 ]
 then
