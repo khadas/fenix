cbca22dbeda10762ca60323c0c135c3ea9de20c2
Frank Wang <frank@khadas.com>
2022-03-31 10:00:37 +0800

    packages: rkbin: update rkbin tools
    add rk3588 support
    
    Signed-off-by: Frank Wang <frank@khadas.com>

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="rkbin"
-PKG_VERSION="5aa9a92b5652d37b323c870329103e120dfc8d87"
-PKG_SHA256="2b8f817e90ce623bf2cae5e6249f14ffc7ac9991e1e44b9ad6b43ca34c57663c"
+PKG_VERSION="750302a720d6074ead976a8822ed38c7bdd341bf"
+PKG_SHA256="581aee16d7fad7c0472522c10e3ddb82429918109c76dedaabd61931f7606bb2"
 PKG_SOURCE_DIR="rkbin-${PKG_VERSION}*"
-PKG_SITE="$GITHUB_URL/rockchip-linux/rkbin"
+PKG_SITE="$GITHUB_URL/numbqq/rkbin"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_ARCH="X86"
 PKG_LICENSE="GPL"
@@ -12,10 +12,16 @@ PKG_NEED_BUILD="NO"
 
 
 make_host() {
-	chmod +x firmware_merger
+#	chmod +x firmware_merger
 	# Fixup links
+	#For Edge
 	ln -fs bin/rk33/rk3399_ddr_800MHz_v1.24.bin rk3399_ddr.bin
 	ln -fs bin/rk33/rk3399_miniloader_spinor_v1.14.bin rk3399miniloaderall_spi.bin
 	ln -fs bin/rk33/rk3399_miniloader_v1.24.bin rk3399miniloaderall.bin
+	# For Edge2
+	ln -fs bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.04.bin rk3588_ddr.bin
+	ln -fs bin/rk35/rk3588_bl31_v1.12.elf rk3588_bl31.elf
+	ln -fs bin/rk35/rk3588_bl32_v1.06.bin rk3588_bl32.bin
+	ln -fs bin/rk35/rk3588_spl_v1.06.bin rk3588_spl.bin
 }
 
