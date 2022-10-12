fc639ca84040bcee8bab66076acfbcd6879aa13b
Nick Xie <nick@khadas.com>
2022-10-12 15:51:30 +0800

    packages: mpp-rockchip-debs: bump to 51aceff
    Signed-off-by: Nick Xie <nick@khadas.com>

--- packages/gstreamer-rockchip-debs/package.mk
+++ packages/gstreamer-rockchip-debs/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="gstreamer-rockchip-debs"
-PKG_VERSION="9f968fff1517a9687fec0d937cd6500e5c52384a"
-PKG_SHA256="ddcce152b881ced1273bd5b92f8befbc8e08763060bd6246543e9e3de303b4cf"
+PKG_VERSION="51aceff33c7fd33456eb36744f48baf08ccea951"
+PKG_SHA256="251528d6d6776ae700901f27d4cea0183d2ef65da534a171fe6685051e415b11"
 PKG_SOURCE_DIR="gstreamer-rockchip-debs-${PKG_VERSION}*"
 PKG_SITE="$GITHUB_URL/numbqq/gstreamer-rockchip-debs"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
