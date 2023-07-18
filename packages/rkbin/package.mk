a6f33543bb3cb7082b60020a696638c648ee76c2
Nick Xie <nick@khadas.com>
2023-07-18 21:19:45 +0800

    packages: rkbin: version bump to 50f626a
    Signed-off-by: Nick Xie <nick@khadas.com>

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="rkbin"
-PKG_VERSION="27d8af0c9204a6094368e0eeea220841f5853d03"
-PKG_SHA256="30bd945d2b273a7658eded7f03c8742957a84068ea0267628200c7d20424bbd0"
+PKG_VERSION="50f626abcd07bdc663406e00993546a686c77ddc"
+PKG_SHA256="5c1811ec2652386a4bc2f78fddb9622e5609dad6207cb7418087b413020c6c63"
 PKG_SOURCE_DIR="rkbin-${PKG_VERSION}*"
 PKG_SITE="$GITHUB_URL/numbqq/rkbin"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
@@ -14,9 +14,9 @@ PKG_NEED_BUILD="NO"
 make_host() {
 	# Fixup links
 	# For Edge2
-	ln -fs bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.09.bin rk3588_ddr.bin
-	ln -fs bin/rk35/rk3588_bl31_v1.33.elf rk3588_bl31.elf
-	ln -fs bin/rk35/rk3588_bl32_v1.12.bin rk3588_bl32.bin
-	ln -fs bin/rk35/rk3588_spl_v1.11.bin rk3588_spl.bin
+	ln -fs bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin rk3588_ddr.bin
+	ln -fs bin/rk35/rk3588_bl31_v1.38.elf rk3588_bl31.elf
+	ln -fs bin/rk35/rk3588_bl32_v1.13.bin rk3588_bl32.bin
+	ln -fs bin/rk35/rk3588_spl_v1.12.bin rk3588_spl.bin
 }
 
