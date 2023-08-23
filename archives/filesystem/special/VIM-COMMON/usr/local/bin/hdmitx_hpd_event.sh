c39c5a7eea2a41ba2f3064a6af1494e64b9d168a
Nick Xie <nick@khadas.com>
2023-08-23 21:34:05 +0800

    VIM4: fix hdmi hotplug issue for server
    Signed-off-by: Nick Xie <nick@khadas.com>

--- archives/filesystem/special/VIM-COMMON/usr/local/bin/hdmitx_hpd_event.sh
+++ archives/filesystem/special/VIM-COMMON/usr/local/bin/hdmitx_hpd_event.sh
@@ -9,14 +9,27 @@ if systemctl is-enabled resize2fs.service | grep "^enabled$" > /dev/null; then
 	exit
 fi
 
-## Restart the GDM service when hdmitx plugin
+if [ ! -d /usr/share/desktop-base ]; then
+	## For server
+	hdmimode=`cat /sys/class/display/mode`
+	if [ "$hdmimode" == "dummy_l" ]; then
+		drm-setcrtc -d meson -s 0 > /dev/null 2>&1
+		sleep 0.5
+		echo 1 > /sys/class/graphics/fb0/blank
+		sleep 0.5
+		echo 0 > /sys/class/graphics/fb0/blank
+	fi
+else
+	## For desktop
+	## Restart the GDM service when hdmitx plugin
 
-process_name="gdm-wayland-session"
-search_string="/usr/libexec/gdm-wayland-session"
+	process_name="gdm-wayland-session"
+	search_string="/usr/libexec/gdm-wayland-session"
 
-output=$(ps aux | grep "$process_name")
-if ! echo "$output" | grep -q "$search_string"; then
-	systemctl restart gdm3
+	output=$(ps aux | grep "$process_name")
+	if ! echo "$output" | grep -q "$search_string"; then
+		systemctl restart gdm3
+	fi
 fi
 
 exit
