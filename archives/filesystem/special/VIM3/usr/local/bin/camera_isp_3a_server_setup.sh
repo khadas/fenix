d4723c56ef2687b51baa036e2e6891cdb4a35f90
Nick Xie <nick@khadas.com>
2023-08-28 09:54:06 +0800

    VIM3: improve isp service for 5.15 kernel
    Signed-off-by: Nick Xie <nick@khadas.com>

--- archives/filesystem/special/VIM3/usr/local/bin/camera_isp_3a_server_setup.sh
+++ archives/filesystem/special/VIM3/usr/local/bin/camera_isp_3a_server_setup.sh
@@ -7,6 +7,7 @@ DAEMON=/usr/local/bin/iv009_isp
 if [ "$BOARD" != "VIM3" ]; then
 	echo "$BOARD does not support ISP!"
 	systemctl stop camera_isp_3a_server.service
+	systemctl disable camera_isp_3a_server.service
 	exit
 fi
 
