94fd8548e9d051473402052ed8550dfadafaa1b9
Nick Xie <nick@khadas.com>
2023-09-14 17:02:36 +0800

    packages: rkbin: version bump to 4c4e34a
    upgrade to android sdk version, tag: android-13.0-mid-rkr6
    
    commit 4c4e34a4cec9ff2ed25ea3a781653db3aeb0ce5c
    Author: Wang Xiaobin <xb.wang@rock-chips.com>
    Date:   Tue Sep 5 15:01:53 2023 +0800
    
        rv1106: mcu: update gc2093 hpmcu tb bin to v1.70
    
        build from commit:
            rtt:3b74006#hal:d93ef9fd#battery_ipc:7ca1398
    
        build from rt-thread commit in develop branch:
            3b74006: bsp: rockchip: camera: gc2093 increase delay for init sequence
    
        build from hal commit in master branch:
            d93ef9fd: device: rk3568: touch gic everytime in IRQ_HardIrqPreemptHandler
    
        build from battery-ipc commit in master branch:
            7ca1398: fastae: update 2.2.4 version
    
        Signed-off-by: Wang Xiaobin <xb.wang@rock-chips.com>
        Change-Id: I36ba16daf782a11d0d359dc6b1c0a56c5db59e03
    
    Signed-off-by: Nick Xie <nick@khadas.com>

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="rkbin"
-PKG_VERSION="50f626abcd07bdc663406e00993546a686c77ddc"
-PKG_SHA256="5c1811ec2652386a4bc2f78fddb9622e5609dad6207cb7418087b413020c6c63"
+PKG_VERSION="4c4e34a4cec9ff2ed25ea3a781653db3aeb0ce5c"
+PKG_SHA256="9e0702b017e17243bfe3a9db6255ba2265c7423ece535809474e6935af70c592"
 PKG_SOURCE_DIR="rkbin-${PKG_VERSION}*"
 PKG_SITE="$GITHUB_URL/numbqq/rkbin"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
@@ -14,9 +14,9 @@ PKG_NEED_BUILD="NO"
 make_host() {
 	# Fixup links
 	# For Edge2
-	ln -fs bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin rk3588_ddr.bin
-	ln -fs bin/rk35/rk3588_bl31_v1.38.elf rk3588_bl31.elf
-	ln -fs bin/rk35/rk3588_bl32_v1.13.bin rk3588_bl32.bin
+	ln -fs bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.13.bin rk3588_ddr.bin
+	ln -fs bin/rk35/rk3588_bl31_v1.41.elf rk3588_bl31.elf
+	ln -fs bin/rk35/rk3588_bl32_v1.14.bin rk3588_bl32.bin
 	ln -fs bin/rk35/rk3588_spl_v1.12.bin rk3588_spl.bin
 }
 
