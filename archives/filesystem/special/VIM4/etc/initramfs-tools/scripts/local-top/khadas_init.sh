4af65a0b5c9779bdf39d033603f24e7358b86219
Nick Xie <nick@khadas.com>
2023-12-29 15:14:18 +0800

    fixup! VIM3/4/4N: remove panel.service and kahdas-init.sh for kernel 5.15
    Signed-off-by: Nick Xie <nick@khadas.com>

--- archives/filesystem/special/VIM4/etc/initramfs-tools/scripts/local-top/khadas_init.sh
+++ archives/filesystem/special/VIM4/etc/initramfs-tools/scripts/local-top/khadas_init.sh
@@ -5,6 +5,15 @@ if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
 	exit 0
 fi
 
+linux_version=`uname -r`
+major_version=$(echo "$linux_version" | cut -d '.' -f 1,2)
+target_version="5.15"
+
+if [ "$major_version" = "$target_version" ]; then
+	echo "Ignore for 5.15 kernel..."
+	exit 0
+fi
+
 panel_exist=0
 
 for x in $(cat /proc/cmdline); do
