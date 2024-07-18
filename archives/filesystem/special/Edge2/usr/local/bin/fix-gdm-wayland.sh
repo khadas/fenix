51ba9107b42803152a08f371982aea8f739f7e9d
Jacobe Zang <jacobe.zang@wesion.com>
2024-07-18 18:18:16 +0800

    archives/filesystem: Edge2: change desktop to x11 for mainline+noble as temporary plan
    Signed-off-by: Jacobe Zang <jacobe.zang@wesion.com>

--- archives/filesystem/special/Edge2/usr/local/bin/fix-gdm-wayland.sh
+++ archives/filesystem/special/Edge2/usr/local/bin/fix-gdm-wayland.sh
@@ -3,7 +3,10 @@
 [ -f /sys/class/drm/card0-DSI-1/modes ] && exit 0
 
 if systemctl is-active --quiet gdm && grep -Fxq "WaylandEnable=true" /etc/gdm3/custom.conf; then
-	if [ ! -f /tmp/.gdm-wayland-fix ]; then
+	if grep -Fxq "DISTRIB_CODENAME=noble" /etc/lsb-release && [ -f /boot/.next ]; then
+		sed -i 's/^WaylandEnable=true/WaylandEnable=false/' /etc/gdm3/custom.conf
+		systemctl restart gdm
+	elif [ ! -f /tmp/.gdm-wayland-fix ]; then
 		sleep 5
 		pidof gdm-x-session > /dev/null && systemctl restart gdm && touch /tmp/.gdm-wayland-fix
 	fi
