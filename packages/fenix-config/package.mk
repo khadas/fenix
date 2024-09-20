a95a327890b98be61b779bcc718b1d9e00cf3931
Gunjan Gupta <gunjan@wesion.com>
2024-09-20 07:58:50 +0000

    migrate to configng based fenix-config
    Signed-off-by: Gunjan Gupta <gunjan@wesion.com>

--- packages/fenix-config/package.mk
+++ packages/fenix-config/package.mk
@@ -1,11 +1,11 @@
 PKG_NAME="fenix-config"
-PKG_VERSION="de4772153a306df6fc25e8e9c06281f208479017"
-PKG_SHA256="3b201dd8425c50dad89be372e649cf7f510e5f3e4eda9c7b633b6913635bbd47"
+PKG_VERSION="a510ad225bcc4850d65e0001f482ca9039a8fece"
+PKG_SHA256="4291271eaa5eb82cb8bbacd0492e5f94479e2ef457ce69cccc920cc39fa89fe9"
 PKG_SOURCE_DIR="${PKG_NAME}-${PKG_VERSION}*"
 PKG_SITE="$GITHUB_URL/numbqq/${PKG_NAME}"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_ARCH="arm aarch64"
-PKG_LICENSE="GPL"
+PKG_LICENSE="GPL 2.0"
 PKG_SHORTDESC="Linux configuration utility"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_NEED_BUILD="YES"
@@ -22,23 +22,16 @@ make_target() {
 	Version: ${VERSION}
 	Architecture: all
 	Maintainer: Khadas <hello@khadas.com>
-	Depends: bash, bc, build-essential, curl, dialog, debconf, debconf-utils, dirmngr, expect, html2text, iperf3, jq, psmisc, pv, software-properties-common, unzip, zip
-	Suggests: libpam-google-authenticator, qrencode, network-manager
+	Depends: bash, jq, whiptail
+	Suggests: libpam-google-authenticator, qrencode, network-manager, netplan.io
 	Section: utils
 	Priority: optional
 	Description: Fenix configuration utility
 	EOF
 
-	mkdir -p "${pkgdir}"/usr/{bin,lib/fenix-config,sbin}
-	install -m 755 fenix-config "${pkgdir}"/usr/sbin/fenix-config
-	install -m 644 fenix-config-jobs "${pkgdir}"/usr/lib/fenix-config/jobs.sh
-	install -m 644 fenix-config-submenu "${pkgdir}"/usr/lib/fenix-config/submenu.sh
-	install -m 644 fenix-config-functions "${pkgdir}"/usr/lib/fenix-config/functions.sh
-	install -m 644 fenix-config-functions-network "${pkgdir}"/usr/lib/fenix-config/functions-network.sh
-	install -m 755 softy "${pkgdir}"/usr/sbin/softy
-
-	ln -sf /usr/sbin/fenix-config "${pkgdir}"/usr/bin/fenix-config
-	ln -sf /usr/sbin/softy "${pkgdir}"/usr/bin/softy
+	mkdir -p "${pkgdir}"/usr/{bin,lib/fenix-config}
+	find bin lib -exec install -m 644 {} "${pkgdir}"/usr/{} \;
+	chmod a+x "${pkgdir}"/usr/bin/fenix-config
 
 	info_msg "Building package: $pkgname"
 	fakeroot dpkg-deb -b -Zxz $pkgdir ${pkgdir}.deb
