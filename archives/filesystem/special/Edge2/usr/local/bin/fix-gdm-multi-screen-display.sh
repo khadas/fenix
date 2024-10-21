1e60a3ebb7c69cb3e2cb78b71cd45b56f53b17f4
Jacobe Zang <jacobe.zang@wesion.com>
2024-10-21 14:51:36 +0800

    archives/filesystem: Edge2: HDMI cannot sleep for hotplug function
    Signed-off-by: Jacobe Zang <jacobe.zang@wesion.com>

--- archives/filesystem/special/Edge2/usr/local/bin/fix-gdm-multi-screen-display.sh
+++ archives/filesystem/special/Edge2/usr/local/bin/fix-gdm-multi-screen-display.sh
@@ -16,14 +16,6 @@ if [ -f /sys/devices/platform/display-subsystem/drm/card0/card0-DP-1/status ]; t
     fi
 fi
 
-if [ -f /sys/devices/platform/display-subsystem/drm/card0/card0-HDMI-A-1/status ]; then
-    HDMI_A1_STATUS=`cat /sys/devices/platform/display-subsystem/drm/card0/card0-HDMI-A-1/status`
-    if [ "connected" = "$HDMI_A1_STATUS" ]; then
-        echo off > /sys/devices/platform/display-subsystem/drm/card0/card0-HDMI-A-1/status
-        HDMI_PRE_OFF="true"
-    fi
-fi
-
 sleep 2
 
 if [ -f /sys/devices/platform/display-subsystem/drm/card0/card0-DSI-1/status ]; then
@@ -37,12 +29,5 @@ if [ -f /sys/devices/platform/display-subsystem/drm/card0/card0-DP-1/status ]; t
     ON_DP1_STATUS=`cat /sys/devices/platform/display-subsystem/drm/card0/card0-DP-1/status`
     if [ "disconnected" = "$ON_DP1_STATUS" ] && [ "true" = "$DP1_PRE_OFF" ]; then
         echo on > /sys/devices/platform/display-subsystem/drm/card0/card0-DP-1/status
-    fi
-fi
-
-if [ -f /sys/devices/platform/display-subsystem/drm/card0/card0-HDMI-A-1/status ]; then
-    ON_HDMI_A1_STATUS=`cat /sys/devices/platform/display-subsystem/drm/card0/card0-HDMI-A-1/status`
-    if [ "disconnected" = "$ON_HDMI_A1_STATUS" ] && [ "true" = "$HDMI_PRE_OFF" ]; then
-        echo on > /sys/devices/platform/display-subsystem/drm/card0/card0-HDMI-A-1/status
     fi
 fi
