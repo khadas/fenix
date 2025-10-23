a16e3fd6fcf1d00deb38fe9e7552a94064a0dd09
Nick Xie <nick@khadas.com>
2025-10-23 10:53:58 +0800

    packages: rkbin: Edge 2L: add usbplug patch
    rk3576: usbplug: Update for Edge 2L
    
    Build from u-boot:
    		7243b1befe scripts: uboot.sh: fix rkbib path
    		2ce0258155 rockchip: dts: rk3576: fix spi flash
    		8bf2cff472 rockchip: dts: rk3576: adjust spl-boot-order
    		15ce87effb Edge 2L: set VCC_5V_EN and TYPEC_POWER_EN.
    		42b42e591d Edge 2L: skip some display error while booting
    		16ad73f45d arm64: defconfig: Edge 2L: unset PHYLIB.
    
    Build command:
            ./make.sh rk3576-usbplug
    
    Update feature:
            Add FSPI1 support.
    
    Signed-off-by: Nick Xie <nick@khadas.com>
    Change-Id: I32c667cf85bb76778f029868e273e3c33b4939f0

--- packages/rkbin/package.mk
+++ packages/rkbin/package.mk
@@ -24,5 +24,8 @@ make_host() {
 	ln -fs bin/rk35/rk3576_bl31_v1.21.elf rk3576_bl31.elf
 	ln -fs bin/rk35/rk3576_bl32_v1.07.bin rk3576_bl32.bin
 	ln -fs bin/rk35/rk3576_spl_v1.08.bin rk3576_spl.bin
+	## Fix usbplug, replace bin/rk35/rk3576_usbplug_v*.bin with bin/rk35/rk3576_usbplug_edge-2l.bin
+	sed -i '/\[CODE472_OPTION\]/,/^$/ s|^Path1=bin\/rk35\/rk3576_usbplug_.*\.bin$|Path1=bin/rk35/rk3576_usbplug_edge-2l.bin|' RKBOOT/RK3576MINIALL.ini
+	cp $PKGS_DIR/$PKG_NAME/files/Edge-2L/rk3576_usbplug_edge-2l.bin bin/rk35/
 }
 
