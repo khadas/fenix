0f3a622bdff3c96d96ddc54d3296f263d3ab6ab5
Nick Xie <nick@khadas.com>
2025-10-28 17:02:56 +0800

    filesystem: Edge 2L: fix sound card name
    Change-Id: Iac9a86e19c5a23ae3b4df8157c90184c092dbc23

--- archives/filesystem/special/Edge-2L/etc/wireplumber/main.lua.d/51-hdmi.lua
+++ archives/filesystem/special/Edge-2L/etc/wireplumber/main.lua.d/51-hdmi.lua
@@ -1,26 +1,12 @@
 rule = {
     matches = {
         {
-            { "device.bus-path", "equals", "platform-hdmi0-sound" },
+            { "device.bus-path", "equals", "platform-hdmi-sound" },
         },
     },
     apply_properties = {
         ["device.description"] = "HDMI",
         ["node.description"] = "HDMI",
-    },
-}
-
-table.insert(alsa_monitor.rules,rule)
-
-rule = {
-    matches = {
-        {
-            { "alsa.card_name", "equals", "rockchip-hdmi0" },
-        },
-    },
-    apply_properties = {
-        ["priority.driver"] = "1500",
-        ["priority.session"] = "1500",
     },
 }
 
